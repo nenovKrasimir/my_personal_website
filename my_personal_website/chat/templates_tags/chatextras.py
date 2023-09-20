62b01702b604cf80a08227d78869816aa177123a
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-09-21 00:15:02 +0300

    adjusting chat apperance

--- my_personal_website/chat/templates_tags/chatextras.py
+++ my_personal_website/chat/templates_tags/chatextras.py
@@ -5,8 +5,11 @@ register = template.Library()
 
 @register.filter(name='initials')
 def initials(value):
-    initials = value[0]+value[1]
 
+    if len(value) >= 2:
+        initials = value[0]+value[1]
+    else:
+        initials = value
     # for name in value.split(' '):
     #     if name and len(initials) < 3:
     #         initials += name[0].upper()
